b1c9c3daaa0f2e33f4b7d49056122547f658a5ff
SosigDev <thelamps50@gmail.com>
2023-10-05 22:31:01 -0400

    movement system changed not done i iwll continu tmr so dw

--- 2D fighting game-Rage fist/Assets/prefab/scripts/movement.cs
+++ 2D fighting game-Rage fist/Assets/prefab/scripts/movement.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class movement : MonoBehaviour
 {
+    [Header("Controls")]
+    private PlayerInput playerControls;
 
+    [Header("Walking")]
     public float speed = 1f;
     [SerializeField] float speedDivident = 100f;
 
@@ -15,35 +19,27 @@ public class movement : MonoBehaviour
     public bool isGrounded;
     public LayerMask anythingMask;
 
+    [Header("Jumping")]
     public float yVelocity = 0.0001f;
     [SerializeField] float currentYVel;
 
     public float jumpYVel = 0.01f;
     public float jumpYVelStartup;
 
+    //Controls (Awake gets called before Start)
+    private void Awake()
+    {
+        playerControls = new PlayerInput();
 
-    void Start()
+        //playerControls.Player.Move.performed += 
+    }
+
+    private void Start()
     {
         jumpYVelStartup = groundDistance;
     }
 
-    
-    void MovementRightLeft()
-    {
-        if(Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(speed/speedDivident, 0f, 0f);
-            Debug.Log("going right");
-        }
-        
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(-speed/speedDivident, 0f, 0f);
-            Debug.Log("going left");
-        }
-    }
-
-    void Update()
+    private void Update()
     {
         IsGrounded();
         MovementRightLeft();
@@ -51,32 +47,51 @@ public class movement : MonoBehaviour
         Fall();
     }
 
-    void Fall()
+    // Functions
+    void MovementRightLeft()
     {
-        transform.Translate(0,currentYVel,0);
 
-        if(!isGrounded)
+
+        if (Input.GetKey(KeyCode.D))
         {
-            currentYVel = currentYVel - yVelocity;
+            transform.Translate(speed / speedDivident, 0f, 0f);
+            Debug.Log("going right");
         }
-        else
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-speed / speedDivident, 0f, 0f);
+            Debug.Log("going left");
+        }
+    }
+
+    private void Fall()
+    {
+        transform.Translate(0, currentYVel, 0);
+
+        if (isGrounded) 
         {
             currentYVel = 0;
         }
-    }
 
-    void Jump()
-    {
-        if (isGrounded)
+        else
         {
-            transform.Translate(0,0,0);
-            currentYVel = jumpYVel;
+            currentYVel -= yVelocity;
         }
     }
 
-    void IsGrounded()
+    private void Jump()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, anythingMask);
+        if (isGrounded)
+        {
+            //transform.Translate(0,0,0);
+            //currentYVel = jumpYVel;
+        }
+    }
+
+    private void IsGrounded()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundDistance, anythingMask);
     }
 
     private void OnDrawGizmos()
