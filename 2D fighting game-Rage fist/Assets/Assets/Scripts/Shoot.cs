045ed8da6fdc12b99e6127274ed95d4bbfb809b1
SosigDev <thelamps50@gmail.com>
2023-12-31 11:19:13 -0500

    fixed ur particles

--- 2D fighting game-Rage fist/Assets/Assets/Scripts/Shoot.cs
+++ 2D fighting game-Rage fist/Assets/Assets/Scripts/Shoot.cs
@@ -1,17 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Shoot : MonoBehaviour
+/// <summary>
+/// Making multiple functions for all of the type of weapons example of weapons: bow, rocket, bullet, etc... we can add more if we wanted to...
+/// A parameter in the functions will be set to identify wether the weapon is a automatic, burst or semi auto
+/// </summary>
+
+public enum FiringModes
 {
+    SemiAutomatic,
+    Automatic,
+    Burst
+};
 
-    [SerializeField] ParticleSystem boolet;
+public enum ProjectileTypes
+{
+    Bullet,
+    Rocket,
+    Bow
+};
 
-    void Update()
+public class GunController : MonoBehaviour
+{
+    private PlayerControls playerControls;
+
+    public GameObject bullet;
+
+    private void Awake()
     {
-        if(Input.GetButtonDown("Fire1"))
+        playerControls = new PlayerControls();
+
+        playerControls.Player.Shoot.performed += ctx => ShootGun(FiringModes.SemiAutomatic);
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    private void CreateBullet(ProjectileTypes bulletType)
+    {
+        switch (bulletType)
         {
-            boolet.Play();
+            case ProjectileTypes.Bullet:
+                Instantiate(bullet, transform.position, transform.rotation);
+
+                break;
+
+            case ProjectileTypes.Rocket:
+                // rocket
+                break;
+
+            case ProjectileTypes.Bow:
+                // bow
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public void ShootGun(FiringModes firingMode)
+    {
+        switch (firingMode)
+        {
+            case FiringModes.SemiAutomatic:
+                CreateBullet(ProjectileTypes.Bullet);
+
+                break;
+
+            case FiringModes.Automatic:
+                // automatic
+                break;
+
+            case FiringModes.Burst:
+                // burst
+                break;
+
+            default:
+                break;
         }
     }
 }
